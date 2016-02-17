a883427143b399398d719b0be89d719950b252c1
Ghayyas <ghayyasmubashir@gmail.com>
2016-02-17 21:54:54 +0500

    inclduing bootstrap

--- public/Mobile/salesman/www/components/home/home.js
+++ public/Mobile/salesman/www/components/home/home.js
@@ -1,8 +1,9 @@
 angular.module('starter')
 .controller('homeCtrl',function($rootScope,$http,$scope,local,$firebaseArray){
     var ab = localStorage.getItem('usertoken');  
+    var cd = localStorage.getItem('token');
    $scope.user = {ab:ab};
-    var ref = new Firebase("https://salesmanapp1.firebaseio.com/");
+    var ref = new Firebase("https://salesmanapp1.firebaseio.com/").child(cd);
     var ref2 = ref.child('msg');
     $scope.ac = $firebaseArray(ref2);
 //    $rootScope.order = []; 
@@ -11,6 +12,11 @@ angular.module('starter')
 //        console.log(msg);
 //    })
    $scope.submit = function(){
+     $scope.ac.$add({
+           orders: $scope.user.order,
+           quantity:$scope.user.quantity,
+           user: $scope.user.ab
+       })
    $http.post(local + '/ordersend',$scope.user).then(function(success){
        console.log('success',success);
        console.log("sdata",success.data.data)
@@ -18,10 +24,7 @@ angular.module('starter')
        console.log('order',success.data.data.ordersPlace);
        $scope.quantity = success.data.data.quantity;
        console.log('quantity',success.data.data.quantity)
-       $scope.ac.$add({
-           orders: $scope.ordes,
-           quantity:$scope.quantity
-       })
+     
        
        
    },function(err){
--- public/components/orderlist/orderlist.js
+++ public/components/orderlist/orderlist.js
@@ -1,23 +1,39 @@
 angular.module('app.getorder',[])
-.controller('getCtrl',function($scope,$http,local){
+.controller('getCtrl',function($scope,$http,local,$rootScope,$firebaseArray){
     var ab = localStorage.getItem('token');
     //$scope.fb = {FirebaseToken: ab}
    // console.log('ab');
-   var ref = new Firebase("https://salesmanapp1.firebaseio.com/");
+   var ref = new Firebase("https://salesmanapp1.firebaseio.com/").child(ab);
+   console.log("Hello",ref)
     var ref2 = ref.child('msg');
-      $scope.order = [];
+      //$scope.order = [];
+
+              $scope.aer = false;      
+            
             ref2.on('child_added', function(message) {
-                $scope.order.push(message.val().msg)
+             //   $scope.order.push(message.val().msg)
+                         $scope.aer = true;
+                console.log("Hello",ref2)
                 console.log('sfsdf',message.val());
-                console.log('orders are',message.val().orders[0]);
-                console.log('quantities are ',message.val().quantity[2]);
-               // alert(message.val());
-               alert('orders are',message.val().orders[0]);
-               alert('quantities are ',message.val().quantity[0]);
+               // console.log('orders are',message.val().orders[0]);
+                //console.log('quantities are ',message.val().quantity[2]);
+                ab = message.val();
+                $scope.cd = ab.orders;
+                $scope.q = ab.quantity;
+                $scope.msg = $firebaseArray(ref2);
+
+            // console.log('abce',$scope.msg);
+                alert("Order Recived : " + message.val().orders + " Quantities Are : " + message.val().quantity);
+               //alert('orders are',message.val().orders[0]);
+               //alert('quantities are ',message.val().quantity[0]);
                 })
-                
+
+                       console.log('cd',$scope.cd);
     $http.get(local+'/getorders?token='+ab).then(function(success){
-        console.log("data",success);
+        console.log("data",success)
+        $scope.data = success.data.data;
+        $scope.aer = false;
+        console.log('sdf',$scope.data);
         ref.remove();
     },function(err){
         console.log("error",err);
